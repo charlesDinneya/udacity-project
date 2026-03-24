4b633f555c21eaaf334d030da33fff1fa0672cbc
charlesDinneya <charles.chinaemerem.dinneya@cariad.technology>
2026-03-24 09:51:28 +0100

    docs: Update bikeshare.py
    Updated the bikeshare.py script to include the description of the default csv file finder

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,9 @@ import time
 import pandas as pd
 import numpy as np
 
+"""
+This is a default dictionary for accessing the csv files
+"""
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
